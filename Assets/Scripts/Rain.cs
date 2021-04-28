4c355c1abe958fef9a804b439c0c88fab9a1d8bd
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2021-04-28 12:13:34 +0100

    Track updates

---
+++ Assets/Scripts/Rain.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Attach to camera
+public class Rain : MonoBehaviour
+{
+    public GameObject shower;
+
+    void Update()
+    {
+        Vector3 camPos = gameObject.transform.position;
+        shower.transform.position = new Vector3(camPos.x, camPos.y + 1.5f, camPos.z);
+    }
+}
